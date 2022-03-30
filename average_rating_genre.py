3a5ba677df9c7589667c4cf3a9c6c4f8f44689bb
Ciaran <C.Lunt@2020.ljmu.ac.uk>
2022-03-30 21:33:24 +0100

    update

--- average_rating_genre.py
+++ average_rating_genre.py
@@ -1,7 +1,8 @@
 import csv
+from lib2to3.pgen2.token import EQUAL
 import statistics
 # csv file name
-filename = 'regex_imdb.csv'
+filename = 'C:/Users/csmclunt/OneDrive - Liverpool John Moores University/csws(Team 4)/csws-week3/regex_imdb.csv'
  
 # initializing the titles and rows list
 fields = []
@@ -26,4 +27,14 @@ with open(filename, 'r', encoding='utf-8') as csvfile:
  #printing the field names
 print('Field names are:' + ', '.join(field for field in fields))
  
+rating_list = []
+with open("C:/Users/csmclunt/OneDrive - Liverpool John Moores University/csws(Team 4)/csws-week3/regex_imdb.csv", "r") as f:
+    for row in rows:
+        rating = row[4]
+        if rating is not None and rating is not "--":
+            rating_list.append(float(row[rating]))
+avg_rating = sum(rating_list) / len(rating_list)
+print ("avg of rating: ", avg_rating)
+
+   
 
