311438edac6f0cdc9f546eecb995df1d9029130e
Ciaran <C.Lunt@2020.ljmu.ac.uk>
2022-03-31 20:52:47 +0100

    basic info v2

--- basic_info.py
+++ basic_info.py
@@ -7,7 +7,15 @@ warnings.simplefilter("ignore")
 from IPython.display import display
 
 df=pd.read_csv("regex_imdb.csv")
-# pd.set_printoptions(max_columns=10)
+df["Meta_score"]=df["Meta_score"].fillna(df["Meta_score"].median())
+df["Meta_score"]=df["Meta_score"].apply(np.round)
+df["Gross"]=df["Gross"].fillna(df["Gross"].median())  #impute missing values in meta-score, director etc 
+df["Gross"]=df["Gross"].apply(np.round,decimals=2)     #to make analyzing more efficent
+df["Director"]=df["Director"].fillna(df["Director"].mode()[0])
+df.isna().sum()
+
+df["Meta_score"]=df["Meta_score"].astype("int") #changes row meta_score to an int instead 
+                                                 #  of a float for easier processing
 
 df.head() #displays first 4 rows of data set
 # display(df.head())
@@ -18,10 +26,4 @@ print(df.dtypes)
 df.info() # displays non-null count
 print(df.info)
 
-df["Meta_score"]=df["Meta_score"].fillna(df["Meta_score"].median())
-import numpy as np
-df["Meta_score"]=df["Meta_score"].apply(np.round)
-df["Gross"]=df["Gross"].fillna(df["Gross"].median())  #impute missing values in meta-score, director etc 
-df["Gross"]=df["Gross"].apply(np.round,decimals=2)     #to make analyzing more efficent
-df["Director"]=df["Director"].fillna(df["Director"].mode()[0])
-df.isna().sum()
+
