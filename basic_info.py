ce79b1a6a89299a87487e89564fafd8540e56f80
Ciaran <C.Lunt@2020.ljmu.ac.uk>
2022-03-31 20:44:24 +0100

    Basic displays

---
+++ basic_info.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import warnings
+import numpy as np
+warnings.simplefilter("ignore")
+from IPython.display import display
+
+df=pd.read_csv("regex_imdb.csv")
+# pd.set_printoptions(max_columns=10)
+
+df.head() #displays first 4 rows of data set
+# display(df.head())
+
+df.dtypes  #displays the data types 
+print(df.dtypes)
+
+df.info() # displays non-null count
+print(df.info)
+
+df["Meta_score"]=df["Meta_score"].fillna(df["Meta_score"].median())
+import numpy as np
+df["Meta_score"]=df["Meta_score"].apply(np.round)
+df["Gross"]=df["Gross"].fillna(df["Gross"].median())  #impute missing values in meta-score, director etc 
+df["Gross"]=df["Gross"].apply(np.round,decimals=2)     #to make analyzing more efficent
+df["Director"]=df["Director"].fillna(df["Director"].mode()[0])
+df.isna().sum()
