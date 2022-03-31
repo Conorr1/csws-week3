9e30eefa8794da5137e54228e49abc7548c19f3b
Ciaran <C.Lunt@2020.ljmu.ac.uk>
2022-03-31 21:20:55 +0100

    finished

--- average_runtime_by_year.py
+++ average_runtime_by_year.py
@@ -19,12 +19,12 @@ avg_runtime_min=avg["mean"]-avg["std"]
 avg_runtime_max=avg["mean"]+avg["std"]
 
 fig, x=plt.subplots(figsize=(10,5))
-x.plot(avg_runtime,color='red')
+x.plot(avg_runtime,color='red', label="runtime")
 x.plot(avg_runtime_min,color='cyan')
 x.plot(avg_runtime_max,color='green')
 x.fill_between(avg.index,avg_runtime_min,avg_runtime_max,color="white")
-x.set_title("AVERAGE MOVIE RUN-TIME OVER THE YEARS",fontsize=20)
-x.set_xlabel("RELEASE YEAR",fontsize=15)
-x.set_ylabel("MINUTES",fontsize=15)
+x.set_title("Average run-time over the years",fontsize=20)
+x.set_xlabel("Year of release",fontsize=15)
+x.set_ylabel("Time Played in Minutes",fontsize=15)
 x.set_xlim(1960)
 plt.show()
