85607b16cca1cb25ec836317b2b17efb44d5d0d3
Ciaran <C.Lunt@2020.ljmu.ac.uk>
2022-04-01 00:40:54 +0100

    done

---
+++ highest_grossed_movies.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import warnings
+import numpy as np
+import plotly.graph_objects as go
+warnings.simplefilter("ignore")
+from IPython.display import display
+
+df=pd.read_csv("regex_imdb.csv")
+
+nam=df.groupby(df["Name"])["Gross"].max().sort_values(ascending=False).head(10).reset_index()
+nam["Gross"]
+print(nam)
+
+plt.figure(figsize=(10,5))
+x=df.groupby(df["Name"])["Gross"].max().sort_values(ascending=False).head(10)
+sns.barplot(x.values,x.index)
+plt.title("Top Ten Highest Grossed Movies Of All Time")
+plt.show()
+
