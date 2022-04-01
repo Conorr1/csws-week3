aff867fbef18c68e73ed175a30c36c4f18e7a18d
Ciaran <C.Lunt@2020.ljmu.ac.uk>
2022-04-01 01:32:47 +0100

    done

--- average_movie_votes.py
+++ average_movie_votes.py
@@ -6,6 +6,9 @@ import numpy as np
 warnings.simplefilter("ignore")
 from IPython.display import display
 
+def average_movie_votes():
+      pass    
+
 df=pd.read_csv("regex_imdb.csv")
 v=df.groupby(df["Year"])["Votes"].mean().reset_index()
 print(v)
---
+++ top_10_film_director.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import warnings
+import numpy as np
+warnings.simplefilter("ignore")
+from IPython.display import display
+
+df=pd.read_csv("regex_imdb.csv")
+
+x=df.groupby(by=df["Director"])["Gross"].max().sort_values(ascending=False).head(10)
+
+print(x)
+
+plt.title("DIRECTORS WHO HAVE DIRECTED THE TOP  GROSS MOVIES")
+y=sns.barplot(x.index,x.values)
+y.set_xlabel("DIRECTORS")
+plt.xticks(rotation=45)
+plt.show()
