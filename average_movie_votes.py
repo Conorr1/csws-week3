d6c8ee4cc562f055dbba28c43b9b39201736ee67
Ciaran <C.Lunt@2020.ljmu.ac.uk>
2022-04-04 20:59:28 +0100

    done

--- average_movie_votes.py
+++ average_movie_votes.py
@@ -12,9 +12,6 @@ def average_movie_votes():
 df=pd.read_csv("regex_imdb.csv")
 v=df.groupby(df["Year"])["Votes"].mean().reset_index()
 print(v)
-plt.figure(figsize=(10,5))
-sns.lineplot(x="Year",y="Votes",data=v)
-plt.title("Average movie votes over the years",fontsize=20)
-plt.show()
+
    
 
